8ed1c9a55271013152cccd8c00daf70915053182
Vinay <rmdort@gmail.com>
2017-04-20 18:58:40 +0800

    updates

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import cx from 'classnames'
+import CardList from './CardList'
+import CardButton from './CardButton'
 
 const Card = ({ card }) => {
   if (!card) return null
   let { title, url, buttons, template } = card
   let klass = cx('ola-card', `ola-card-template-${template}`)
+
+  function pickTemplate (template) {
+    switch (template) {
+      case 'list':
+        return <CardList {...card} />
+
+      default:
+        return (
+          <div>
+            <h3 className='ola-card-title'><a target='_blank' href={url}>{title}</a></h3>
+            {buttons.map((button, idx) => <CardButton {...button} key={idx} />)}
+          </div>
+        )
+    }
+  }
+
+
   return (
     <div className={klass}>
-      <h3 className='ola-card-title'><a target='_blank' href={url}>{title}</a></h3>
-      {buttons.map(({ title: buttonTitle, type, url: buttonUrl }, idx) => <a key={idx} className='ola-card-button' href={buttonUrl}>{buttonTitle}</a>)}
+      {pickTemplate(template)}
     </div>
   )
 }
