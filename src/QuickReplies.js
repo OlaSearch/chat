1e87830d9df9dd05a8019be17aae7a55248f8c91
Vinay <rmdort@gmail.com>
2017-05-15 13:39:53 +0800

    updates

--- src/QuickReplies.js
+++ src/QuickReplies.js
@@ -10,16 +10,17 @@ class QuickReplies extends Component {
   };
   render () {
     let { options, isActive } = this.props
-    if (!isActive) return null
     if (!options || !options.length) return null
     let replies = options.map(({ label }, idx) => <QuickReplyButton key={idx} label={label} isActive={isActive} handleClick={this.handleClick} />)
+
+    // if (!isActive) replies = null
     return (
       <div className='olachat-qreply'>
         <ReactCSSTransitionGroup
           transitionName='qreply'
           transitionAppear
           transitionEnterTimeout={300}
-          transitionLeaveTimeout={100}
+          transitionLeaveTimeout={300}
           transitionAppearTimeout={300}
           component='div'
           className='olachat-qreply-list'
